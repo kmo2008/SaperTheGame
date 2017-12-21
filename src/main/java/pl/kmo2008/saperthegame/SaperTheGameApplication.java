154f260b85f71c37d50093822251d1851aa8a852
Kmo2008 <kamilostry20@gmail.com>
2017-12-21 16:29:44 +0100

    Initial commit

---
+++ src/main/java/pl/kmo2008/saperthegame/SaperTheGameApplication.java
@@ -0,0 +1,12 @@
+package pl.kmo2008.saperthegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaperTheGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaperTheGameApplication.class, args);
+	}
+}
