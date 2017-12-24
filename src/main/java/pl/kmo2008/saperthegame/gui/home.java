b0a89aad44901047e8d6cadd7e9a0487ea4d2580
Kmo2008 <kamilostry20@gmail.com>
2017-12-24 14:32:37 +0100

    Fix game refreshing

--- src/main/java/pl/kmo2008/saperthegame/gui/home.java
+++ src/main/java/pl/kmo2008/saperthegame/gui/home.java
@@ -4,6 +4,8 @@ import com.vaadin.annotations.Theme;
 import com.vaadin.server.ThemeResource;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.shared.MouseEventDetails;
+import com.vaadin.shared.ui.AlignmentInfo;
+import com.vaadin.shared.ui.MarginInfo;
 import com.vaadin.spring.annotation.SpringUI;
 import com.vaadin.ui.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -42,7 +44,7 @@ public class home extends UI {
         /**
          * Layouts merge
          */
-        gameAndRanks.addComponents(game, ranks);
+        gameAndRanks.addComponents(ranks, game);
         head.addComponents(gameAndRanks);
         /**
          * Content settings
@@ -54,7 +56,13 @@ public class home extends UI {
          */
         //game.setStyleName("gridgame");
         game.setSizeUndefined();
+        head.setSizeFull();
+        game.setHeightUndefined();
+        game.setRows(gameboard.getHeight());
+        game.setColumns(gameboard.getWidth());
         generateMatrixGrid(gameboard.getHeight(), gameboard.getWidth());
+
+
         game.addLayoutClickListener(clickEvent -> {
             Component child = clickEvent.getChildComponent();
             String[] tokens = child.getId().split("x");
@@ -75,7 +83,7 @@ public class home extends UI {
                 if (clickEvent.getButton() == MouseEventDetails.MouseButton.LEFT) {
                     gameboard.revealField(row, col);
                 }
-                generateMatrixGrid(gameboard.getHeight(), gameboard.getWidth());
+                refreshGrid();
             }
 
         });
@@ -83,36 +91,76 @@ public class home extends UI {
 
     }
 
-    private void generateMatrixGrid(final int rows, final int columns) {
-        game.removeAllComponents();
-        game.setRows(rows);
-        game.setColumns(columns);
+    ThemeResource blankfield = new ThemeResource("img/clear-12.png");
+    ThemeResource minenot = new ThemeResource("img/minenotdetected-12.png");
+    ThemeResource one = new ThemeResource("img/1.png");
+    ThemeResource two = new ThemeResource("img/2.png");
+    ThemeResource three = new ThemeResource("img/3.png");
+    ThemeResource four = new ThemeResource("img/4.png");
+    ThemeResource five = new ThemeResource("img/5.png");
+    ThemeResource six = new ThemeResource("img/6.png");
+    ThemeResource seven = new ThemeResource("img/7.png");
+    ThemeResource eigth = new ThemeResource("img/8.png");
+    ThemeResource flagfield = new ThemeResource("img/flagfield-12.png");
+    ThemeResource markfield = new ThemeResource("img/qmark-12.png");
+
+    private void refreshGrid() {
         Field fieldx = null;
         for (int row = 0; row < game.getRows(); row++) {
             for (int col = 0; col < game.getColumns(); col++) {
                 final Component field = new Image();
                 ThemeResource resource = null;
-                fieldx = gameboard.getFieldAt(row, col);
-                if (fieldx.getVisibleState() == VisibleState.UNREVEALED)
-                    resource = new ThemeResource("img/clear-12.png");
-                else if (fieldx.getVisibleState() == VisibleState.REVEALED) {
-                    if (fieldx.getState() == State.EMPTY) resource = null;
-                    else if (fieldx.getState() == State.MINE) resource = new ThemeResource("img/minenotdetected-12.png");
-                    else if (fieldx.getState() == State.ONE) resource = new ThemeResource("img/1.png");
-                    else if (fieldx.getState() == State.TWO) resource = new ThemeResource("img/2.png");
-                    else if (fieldx.getState() == State.THREE) resource = new ThemeResource("img/3.png");
-                    else if (fieldx.getState() == State.FOUR) resource = new ThemeResource("img/4.png");
-                    else if (fieldx.getState() == State.FIVE) resource = new ThemeResource("img/5.png");
-                    else if (fieldx.getState() == State.SIX) resource = new ThemeResource("img/6.png");
-                    else if (fieldx.getState() == State.SEVEN) resource = new ThemeResource("img/7.png");
-                    else if (fieldx.getState() == State.EIGHT) resource = new ThemeResource("img/8.png");
-                } else if (fieldx.getVisibleState() == VisibleState.FLAGGED)
-                    resource = new ThemeResource("img/flagfield-12.png");
-                else if (fieldx.getVisibleState() == VisibleState.QUESTION_MARK)
-                    resource = new ThemeResource("img/qmark-12.png");
+                fieldx = gameboard.getFieldAt(col, row);
+                resource = getFieldResurce(fieldx);
+                Component comp = game.getComponent(col, row);
+                if (comp.getIcon() != resource) {
+                    field.setIcon(resource);
+                    field.setId(col + "x" + row);
+                    game.removeComponent(col, row);
+                    game.addComponent(field, col, row);
+                }
+            }
+        }
+    }
 
+    private ThemeResource getFieldResurce(Field fieldx) {
+        ThemeResource resource = null;
+        if (fieldx.getVisibleState() == VisibleState.UNREVEALED)
+            resource = blankfield;
+        else if (fieldx.getVisibleState() == VisibleState.REVEALED) {
+            if (fieldx.getState() == State.EMPTY) resource = null;
+            else if (fieldx.getState() == State.MINE)
+                resource = minenot;
+            else if (fieldx.getState() == State.ONE) resource = one;
+            else if (fieldx.getState() == State.TWO) resource = two;
+            else if (fieldx.getState() == State.THREE) resource = three;
+            else if (fieldx.getState() == State.FOUR) resource = four;
+            else if (fieldx.getState() == State.FIVE) resource = five;
+            else if (fieldx.getState() == State.SIX) resource = six;
+            else if (fieldx.getState() == State.SEVEN) resource = seven;
+            else if (fieldx.getState() == State.EIGHT) resource = eigth;
+        } else if (fieldx.getVisibleState() == VisibleState.FLAGGED)
+            resource = flagfield;
+        else if (fieldx.getVisibleState() == VisibleState.QUESTION_MARK)
+            resource = markfield;
+        return resource;
+    }
+
+
+    private void generateMatrixGrid(final int rows, final int columns) {
+        game.removeAllComponents();
+        //game.setRows(rows);
+        //game.setColumns(columns);
+        game.setHeight("100%");
+        Field fieldx = null;
+        for (int row = 0; row < game.getRows(); row++) {
+            for (int col = 0; col < game.getColumns(); col++) {
+                final Component field = new Image();
+                ThemeResource resource = null;
+                fieldx = gameboard.getFieldAt(col, row);
+                resource = getFieldResurce(fieldx);
                 field.setIcon(resource);
-                field.setId(row + "x" + col);
+                field.setId(col + "x" + row);
                 game.addComponent(field);
             }
         }
